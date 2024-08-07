364f333286b3e9d3a1409f29ada2cfaf8b780c81
sevu11 <johan.sorman@icloud.com>
2024-08-07 17:03:09 +0200

    upload updated files

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
 #github-action genshdoc
 #
 # @file User
--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -37,9 +37,9 @@ fi
 # set kernel parameter for adding splash screen
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& splash /' /etc/default/grub
 
-echo -e "Installing CyberRe Grub theme..."
+echo -e "Installing Grub theme..."
 THEME_DIR="/boot/grub/themes"
-THEME_NAME=CyberRe
+THEME_NAME=kawaiki
 echo -e "Creating the theme directory..."
 mkdir -p "${THEME_DIR}/${THEME_NAME}"
 echo -e "Copying the theme..."
