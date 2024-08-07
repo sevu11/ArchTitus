74bd0599881d64a7e40bbf197f4cebedb0170dfe
sevu11 <johan.sorman@icloud.com>
2024-08-07 11:28:43 +0200

    Update 1-setup.sh
    Adding LTS kernel to setup script

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -52,11 +52,14 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#sv_SE.UTF-8 UTF-8/sv_SE.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone ${TIMEZONE}
 timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+#localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="sv_SE.UTF-8"
 ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+
 # Set keymaps
 localectl --no-ask-password set-keymap ${KEYMAP}
 
@@ -89,6 +92,32 @@ if [[ ! $DESKTOP_ENV == server ]]; then
     sudo pacman -S --noconfirm --needed ${line}
   done
 fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Kernel  
+-------------------------------------------------------------------------
+"
+# Define kernel type; can be 'default' or 'lts'
+KERNEL_TYPE=${KERNEL_TYPE:-lts}
+
+# Install the appropriate kernel
+if [[ $KERNEL_TYPE == "lts" ]]; then
+  pacman -S --noconfirm --needed linux-lts linux-lts-headers
+else
+  pacman -S --noconfirm --needed linux linux-headers
+fi
+
+# Update bootloader configuration if needed
+echo -ne "
+-------------------------------------------------------------------------
+                    Updating Bootloader  
+-------------------------------------------------------------------------
+"
+if [[ -x "$(command -v grub-mkconfig)" ]]; then
+  sudo grub-mkconfig -o /boot/grub/grub.cfg
+fi
+
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Microcode
@@ -115,7 +144,7 @@ echo -ne "
 gpu_type=$(lspci)
 if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
     pacman -S --noconfirm --needed nvidia
-	nvidia-xconfig
+    nvidia-xconfig
 elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
     pacman -S --noconfirm --needed xf86-video-amdgpu
 elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
@@ -123,43 +152,44 @@ elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
 elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
     pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
 fi
+
 #SETUP IS WRONG THIS IS RUN
 if ! source $HOME/ArchTitus/configs/setup.conf; then
-	# Loop through user input until the user gives a valid username
-	while true
-	do 
-		read -p "Please enter username:" username
-		# username regex per response here https://unix.stackexchange.com/questions/157426/what-is-the-regex-to-validate-linux-users
-		# lowercase the username to test regex
-		if [[ "${username,,}" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
-		then 
-			break
-		fi 
-		echo "Incorrect username."
-	done 
-# convert name to lowercase before saving to setup.conf
-echo "username=${username,,}" >> ${HOME}/ArchTitus/configs/setup.conf
+    # Loop through user input until the user gives a valid username
+    while true
+    do 
+        read -p "Please enter username:" username
+        # username regex per response here https://unix.stackexchange.com/questions/157426/what-is-the-regex-to-validate-linux-users
+        # lowercase the username to test regex
+        if [[ "${username,,}" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
+        then 
+            break
+        fi 
+        echo "Incorrect username."
+    done 
+    # convert name to lowercase before saving to setup.conf
+    echo "username=${username,,}" >> ${HOME}/ArchTitus/configs/setup.conf
 
     #Set Password
     read -p "Please enter password:" password
-echo "password=${password,,}" >> ${HOME}/ArchTitus/configs/setup.conf
+    echo "password=${password,,}" >> ${HOME}/ArchTitus/configs/setup.conf
 
     # Loop through user input until the user gives a valid hostname, but allow the user to force save 
-	while true
-	do 
-		read -p "Please name your machine:" name_of_machine
-		# hostname regex (!!couldn't find spec for computer name!!)
-		if [[ "${name_of_machine,,}" =~ ^[a-z][a-z0-9_.-]{0,62}[a-z0-9]$ ]]
-		then 
-			break 
-		fi 
-		# if validation fails allow the user to force saving of the hostname
-		read -p "Hostname doesn't seem correct. Do you still want to save it? (y/n)" force 
-		if [[ "${force,,}" = "y" ]]
-		then 
-			break 
-		fi 
-	done 
+    while true
+    do 
+        read -p "Please name your machine:" name_of_machine
+        # hostname regex (!!couldn't find spec for computer name!!)
+        if [[ "${name_of_machine,,}" =~ ^[a-z][a-z0-9_.-]{0,62}[a-z0-9]$ ]]
+        then 
+            break 
+        fi 
+        # if validation fails allow the user to force saving of the hostname
+        read -p "Hostname doesn't seem correct. Do you still want to save it? (y/n)" force 
+        if [[ "${force,,}" = "y" ]]
+        then 
+            break 
+        fi 
+    done 
 
     echo "NAME_OF_MACHINE=${name_of_machine,,}" >> ${HOME}/ArchTitus/configs/setup.conf
 fi
@@ -173,24 +203,25 @@ if [ $(whoami) = "root"  ]; then
     useradd -m -G wheel,libvirt -s /bin/bash $USERNAME 
     echo "$USERNAME created, home directory created, added to wheel and libvirt group, default shell set to /bin/bash"
 
-# use chpasswd to enter $USERNAME:$password
+    # use chpasswd to enter $USERNAME:$password
     echo "$USERNAME:$PASSWORD" | chpasswd
     echo "$USERNAME password set"
 
-	cp -R $HOME/ArchTitus /home/$USERNAME/
+    cp -R $HOME/ArchTitus /home/$USERNAME/
     chown -R $USERNAME: /home/$USERNAME/ArchTitus
     echo "ArchTitus copied to home directory"
 
-# enter $NAME_OF_MACHINE to /etc/hostname
-	echo $NAME_OF_MACHINE > /etc/hostname
+    # enter $NAME_OF_MACHINE to /etc/hostname
+    echo $NAME_OF_MACHINE > /etc/hostname
 else
-	echo "You are already a user proceed with aur installs"
+    echo "You are already a user proceed with aur installs"
 fi
+
 if [[ ${FS} == "luks" ]]; then
-# Making sure to edit mkinitcpio conf if luks is selected
-# add encrypt in mkinitcpio.conf before filesystems in hooks
+    # Making sure to edit mkinitcpio conf if luks is selected
+    # add encrypt in mkinitcpio.conf before filesystems in hooks
     sed -i 's/filesystems/encrypt filesystems/g' /etc/mkinitcpio.conf
-# making mkinitcpio with linux kernel
+    # making mkinitcpio with linux kernel
     mkinitcpio -p linux
 fi
 echo -ne "
