c4fdc6d1064c220aae8e79473425d89e5dbf81df
sevu11 <johan.sorman@icloud.com>
2024-08-07 10:20:35 +0200

    Update startup.sh
    Added swedish locale for keyboard.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -249,7 +249,7 @@ keymap () {
 echo -ne "
 Please select key board layout from this list"
 # These are default key maps as presented in official arch repo archinstall
-options=(us by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk)
+options=(by ca cf cz de dk es et fa fi fr gr hu il it lt lv mk nl no pl ro ru sg ua uk us sv)
 
 select_option $? 4 "${options[@]}"
 keymap=${options[$?]}
